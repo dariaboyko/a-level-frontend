0f3f2b61ca48d6725a7c3ae4f2bab573c10b2a3a
dariaboyko <73283727+dariaboyko@users.noreply.github.com>
2021-12-13 13:19:29 +0200

    lecture6

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Snake case (snake_case)
 «Змеиный регистр» — заменяет пробелы на символ подчеркивания.
 Kebab case (kebab-case)
 Его также называют spear-case, и он является стандартом в Lisp.
+
 lecture3:
 Data types:
 string - we can store 0 and more characters.
@@ -16,6 +17,7 @@ null -  when we need an empty value.
 undefined - for unassigned or undefined values.
 symbol - unique and immutable primitive values.
 object - more complex data structure
+
 lecture4:
 Operators:
 arithmetic operators
@@ -27,4 +29,26 @@ Type coercion:
 to string
 to number
 to boolean
+
+lecture5:
+String wrapper
+Strings methods:
+case changing
+searching substrings
+strings splitting
+replacing
+clearing
+
+Number wrapper
+Numbers methods:
+number constants
+number formatting
+
+lecture6:
+objects are structures to gather related data
+to get property/method use .propertyName or [‘propertyName’]
+with descriptors we can describe properties behavior
+to copy objects use Object.assign() or spread operator (...)
+all objects inherit properties and methods from prototypes
+
 
---
+++ js/lecture6/tasks.js
@@ -0,0 +1,93 @@
+function getMinValueOfTwo(value1,value2){
+    if(typeof(value1)===typeof(value2)&&typeof(value2)==="number") {
+        if(value1<value2){
+        return value1;
+        }
+        if(value1===value2){
+            console.log("Your arguments are equal");
+        }
+        else {
+            return value2;
+        }
+    }
+    else{
+        console.log("Your arguments have different or wrong types");
+    }
+}
+
+function getMinValueOfThree(value1,value2,value3){
+    if(typeof(value1)===typeof(value2)&&typeof(value2)===typeof(value3)&&typeof(value3)==="number") {
+        if (value1 < value2 && value1 < value3) {
+            return value1;
+        }
+        if (value2 < value3 && value2 < value1) {
+            return value2;
+        }
+        if (value3 < value2 && value3 < value1) {
+            return value3;
+        }
+        else{
+            console.log("Your arguments are equal");
+        }
+    }
+    else{
+        console.log("Your arguments have different or wrong types");
+    }
+}
+
+function bubble(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    return arr;
+}
+function getMinvalue(array){
+    let count=0;
+    for (let i = 0; i < array.length; i++) {
+        if(typeof (array[i]) === typeof (array[i+1])&&typeof (array[i+1]) ==="number"){
+            count++;
+        }
+    }
+    if (count === array.length - 1){
+        let sortedArray=bubble(array);
+        return sortedArray[0];
+    }
+    else {
+        console.log("Your arguments have different or wrong types");
+    }
+
+}
+function joinObjects(obj1,obj2){
+    if(typeof(obj1)===typeof(obj2)&&typeof(obj2)==="object"){
+        return Object.assign(obj1, obj2);
+    }
+    else {
+        console.log("Your arguments have different or wrong types");
+    }
+}
+const user={
+    name:'Jack',
+    age: 18
+}
+Object.defineProperty(user, "String", {
+    value:`My name is ${user.name}, I am ${user.age}.`
+})
+Object.defineProperty(user, "number", {
+    value:user.age
+})
+console.log("Task1");
+console.log(getMinValueOfTwo(-2,8));
+console.log(getMinValueOfThree(9,1,2));
+console.log(getMinvalue([1,6,2,7,0]));
+console.log("Task2");
+console.log(joinObjects({name:'daria'},{age:18}));
+console.log("Task3");
+console.log("String concatenation: "+user.String);
+console.log(user.number*10);
